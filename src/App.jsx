cec4201d938d3df5179fe04bb1cdfdd4da94f421
royvincent580 <vincentroy580@gmail.com>
2025-07-21 09:43:32 +0300

    Refactor App component to include routing with Dashboard and Home pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
+import GoalCard from './components/GoalCard'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="app">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
